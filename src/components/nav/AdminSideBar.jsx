7c6dcccd3e2ff451edbf79ef60c92ea1897b5ff3
gflexx <glennenri24@gmail.com>
2023-06-18 20:05:15 +0300

    Added active list item

--- src/components/nav/AdminSideBar.jsx
+++ src/components/nav/AdminSideBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import VaccinesIcon from '@mui/icons-material/Vaccines';
@@ -8,6 +8,8 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import CoronavirusIcon from '@mui/icons-material/Coronavirus';
 
 function AdminSideBar(props) {
+    const activeTab = props.activeTab
+    useEffect(() => {}, [activeTab])
     return (
         <Box
             sx={{display: {xs: "none", sm: "block"}}}
@@ -17,6 +19,7 @@ function AdminSideBar(props) {
             <Box position="fixed">
                 <List>
                     <ListItem 
+                        selected={activeTab === 0 ? true : false}
                         onClick={() => props.changeTab(0)}
                         disablePadding>
                         <ListItemButton component='a'>
@@ -27,6 +30,7 @@ function AdminSideBar(props) {
                         </ListItemButton>
                     </ListItem>
                     <ListItem 
+                        selected={activeTab === 1 ? true : false}
                         onClick={() => props.changeTab(1)}
                         disablePadding>
                         <ListItemButton component='a'>
@@ -37,6 +41,7 @@ function AdminSideBar(props) {
                         </ListItemButton>
                     </ListItem>
                     <ListItem 
+                        selected={activeTab === 2 ? true : false}
                         onClick={() => props.changeTab(2)}
                         disablePadding>
                         <ListItemButton component='a'>
@@ -47,6 +52,7 @@ function AdminSideBar(props) {
                         </ListItemButton>
                     </ListItem>
                     <ListItem 
+                        selected={activeTab === 3 ? true : false}
                         onClick={() => props.changeTab(3)}
                         disablePadding>
                         <ListItemButton component='a'>
@@ -57,6 +63,7 @@ function AdminSideBar(props) {
                         </ListItemButton>
                     </ListItem>
                     <ListItem 
+                        selected={activeTab === 4 ? true : false}
                         onClick={() => props.changeTab(4)}
                         disablePadding>
                         <ListItemButton component='a'>
